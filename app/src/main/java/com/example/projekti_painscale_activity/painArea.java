b87fb1a08188e159a165bb353b979318cf6a2510
noriind <103928163+noriind@users.noreply.github.com>
2022-12-05 12:18:42 +0200

    hello

---
+++ app/src/main/java/com/example/projekti_painscale_activity/painArea.java
@@ -0,0 +1,14 @@
+package com.example.projekti_painscale_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class painArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_painarea);
+    }
+}
